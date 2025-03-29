781151a7773e58e97c03b90a468aab2e0fa5ce87
choi <cejewe@naver.com>
2025-03-29 23:16:14 +0900

    vercel hosting

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState, useCallback, useRef } from "react";
 import toast, { Toaster } from "react-hot-toast";
